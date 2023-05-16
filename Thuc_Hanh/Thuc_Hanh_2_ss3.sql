db9129453224e89fc931546f6795d02ff27bc5c0
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-16 11:34:32 +0700

    nop bai tap ss3

--- Thuc_Hanh/Thuc_Hanh_2_ss3.sql
+++ Thuc_Hanh/Thuc_Hanh_2_ss3.sql
@@ -16,3 +16,25 @@ FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.Su
 SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
 FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
 WHERE Sub.SubName = 'CF';
+
+select StudentName 
+from student
+where studentName like 'H%';
+
+select StartDate
+from Class
+where month(StartDate) = 12;
+
+select credit 
+from subject
+where credit >= 3 and credit <= 5;
+
+SET SQL_SAFE_UPDATES = 0;
+update student
+set classId = 2
+where student.studentName = 'Hung';
+SET SQL_SAFE_UPDATES = 1;
+
+select StudentName, SubName,Mark
+from student, subject,mark
+order by Mark DESC , SubName ASC;
