2dccd414d11c1f98c91d4dd86b4572a8eab150ac
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-16 10:43:23 +0700

    thuc hanh 2 ss3

---
+++ Thuc_Hanh/Thuc_Hanh_2_ss3.sql
@@ -0,0 +1,18 @@
+select* 
+from Student;
+select *
+from student
+where status = true;
+select *
+from suBject
+where Credit < 10;
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID;
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID
+WHERE C.ClassName = 'A1';
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId;
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
