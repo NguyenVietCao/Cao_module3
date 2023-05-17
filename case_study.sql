e8aa58bd2002b26339f64ca13526808b5047f3b2
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-17 16:34:33 +0700

    nop case_study

--- case_study.sql
+++ case_study.sql
@@ -173,3 +173,33 @@ value
 (1	,1,	3),
 (2	,1,	2),
 (2	,12,2);
+use case_study;
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+select *
+from nhan_vien
+where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%'
+and length(ho_ten)<= 15;
+-- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select *
+from khach_hang
+where (year(current_date() - year(ngay_sinh))) between 18 and 50
+and dia_chi in('Đà Nẵng','Quảng Trị');
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
+-- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select khach_hang.ho_ten, hop_dong.ma_khach_hang , count(*) as kh_booking
+from hop_dong 
+join khach_hang  on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+ left join loai_khach  on loai_khach.ma_loai_khach = khach_hang.Ma_loai_khach
+where loai_khach.ma_loai_khach in(select ma_loai_khach from loai_khach where ten_loai_khach = 'Diamond')
+group by ma_khach_hang
+order by kh_booking asc;
+-- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, 
+-- tong_tien (Với tổng tiền được tính theo công thức như sau: 
+-- Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng.
+-- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+select * 
+from khach_hang join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+ join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+
