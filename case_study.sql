add1b1e1c9260b376090738378c614fab4b0b5ce
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-17 21:43:57 +0700

    case ngay 17/5/2023

--- case_study.sql
+++ case_study.sql
@@ -213,5 +213,62 @@ from khach_hang
  join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang 
  join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
 join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+group by hop_dong.ma_hop_dong;
+-- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+-- chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+select
+ dich_vu.ma_dich_vu,
+ dich_vu.ten_dich_vu,
+ dich_vu.dien_tich,
+ dich_vu.chi_phi_thue,
+ loai_dich_vu.ten_loai_dich_vu,
+ hop_dong.ma_dich_vu
+ from dich_vu
+ join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+ join hop_dong on hop_dong.ma_dich_vu = loai_dich_vu.ma_loai_dich_vu
+ where dich_vu.ma_loai_dich_vu not in(
+ select hop_dong.ma_dich_vu
+ from hop_dong
+ where month(hop_dong.ngay_lam_hop_dong)in (1,2,3)and year (hop_dong.ngay_lam_hop_dong) = 2021);
+ -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+ -- đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+ select
+ dich_vu.ma_dich_vu,
+ dich_vu.ten_dich_vu,
+ dich_vu.dien_tich,
+ dich_vu.so_nguoi_toi_da,
+ dich_vu.chi_phi_thue,
+ loai_dich_vu.ten_loai_dich_vu
+ from dich_vu
+ join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+ join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+ where year(hop_dong.ngay_lam_hop_dong) = 2020 and year(hop_dong.ngay_lam_hop_dong) not in (2021);
+ -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+ select distinct ho_ten
+ from khach_hang;
+ 
+-- 9.	Thực hiện thống kê doanh thu theo tháng,
+--  nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+select month (hop_dong.ngay_lam_hop_dong) as doanh_thu_thang, count(distinct khach_hang.ma_khach_hang) as sl_kh
+from hop_dong 
+join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+where year(hop_dong.ngay_lam_hop_dong) = 2021
+group by doanh_thu_thang
+order by doanh_thu_thang;
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, 
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+select
+hop_dong.ma_hop_dong, 
+hop_dong.ngay_lam_hop_dong,
+ hop_dong.ngay_ket_thuc,
+ hop_dong.tien_dat_coc, 
+-- so_luong_dich_vu_di_kem 
+sum(dich_vu_di_kem.ma_dich_vu_di_kem) as sl_dv_dk
+from hop_dong
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by hop_dong.ma_hop_dong
+
+
 
