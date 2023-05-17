d50141eb68a71bef9ab910c22357f3638ba10a35
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-17 23:36:59 +0700

    case study 17/5/2023

--- case_study.sql
+++ case_study.sql
@@ -268,7 +268,71 @@ sum(dich_vu_di_kem.ma_dich_vu_di_kem) as sl_dv_dk
 from hop_dong
 join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
 join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
-group by hop_dong.ma_hop_dong
+group by hop_dong.ma_hop_dong;
+
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” 
+-- và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+select 
+dich_vu_di_kem.ma_dich_vu_di_kem,loai_khach.ten_loai_khach
+from dich_vu_di_kem
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+join hop_dong on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
+where loai_khach.ten_loai_khach = 'Diamond' and khach_hang.dia_chi in ('Vinh','Quang Ngai');
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng),
+--  so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem 
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
+-- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
+-- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select 
+hop_dong.ma_hop_dong,
+nhan_vien.ho_ten,
+khach_hang.ho_ten,
+khach_hang.so_dien_thoai,
+dich_vu.ten_dich_vu,
+sum(dich_vu_di_kem.ma_dich_vu_di_kem) as so_luong_dich_vu_di_kem,
+hop_dong.tien_dat_coc
+from hop_dong
+join nhan_vien on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+where month(hop_dong.ngay_lam_hop_dong)in (10,11,12) and year (hop_dong.ngay_lam_hop_dong) = 2020 and hop_dong.ma_hop_dong
+not in (select ma_hop_dong from hop_dong where month(hop_dong.ngay_lam_hop_dong) in (1,2,3) and year (hop_dong.ngay_lam_hop_dong) = 2021)
+group by hop_dong.ma_hop_dong, nhan_vien.ho_ten,khach_hang.ho_ten,khach_hang.so_dien_thoai,dich_vu.ten_dich_vu,hop_dong.tien_dat_coc;
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select 
+dvdk.ma_dich_vu_di_kem,
+dvdk.ten_dich_vu_di_kem,
+count(*) as so_lan_su_dung
+from dich_vu_di_kem as dvdk
+join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join hop_dong hd on hd.ma_hop_dong = hdct.ma_hop_dong
+group by dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem
+order by so_lan_su_dung desc;
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, 
+-- ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select
+hd.ma_hop_dong,
+ldv.ten_loai_dich_vu,
+dvdk.ten_dich_vu_di_kem,
+count(dvdk.ma_dich_vu_di_kem) as so_lan_su_dung
+from hop_dong hd
+join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+group by hd.ma_hop_dong, ldv.ten_loai_dich_vu,dvdk.ten_dich_vu_di_kem
+having count (dvdk.ma_dich_vu_di_kem) = 1
+
+
+
+
 
 
 
