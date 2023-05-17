c90fca86b6ad8dd8a4628f1f37ce31185d12ac02
Mac <mac@MacBook-Pro-cua-Cao.local>
2023-05-17 18:38:02 +0700

    case study

--- case_study.sql
+++ case_study.sql
@@ -199,7 +199,19 @@ order by kh_booking asc;
 -- Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng.
 -- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 
-select * 
-from khach_hang join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
- join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+select khach_hang.ma_khach_hang,
+khach_hang.ho_ten,
+loai_khach.ten_loai_khach,
+hop_dong.ma_hop_dong,
+dich_vu.ten_dich_vu,
+hop_dong.ngay_lam_hop_dong,
+hop_dong.ngay_ket_thuc,
+sum(dich_vu.chi_phi_thue + isnull(hop_dong_chi_tiet.so_luong * dich_vu.chi_phi_thue)) as tong_tien
+
+from khach_hang
+ join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+ join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang 
+ join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+group by hop_dong.ma_hop_dong
 
